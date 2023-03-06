bd48ac44b285ddb798aa5e7075e14cfa808191a0
Reginald Graham <reg.graham98@gmail.com>
2023-03-06 17:07:35 -0600

    clauses

---
+++ group_by_exercises.sql
@@ -0,0 +1,30 @@
+use employees;
+
+select * from titles;
+
+select distinct title
+from titles;
+
+select first_name, last_name
+from employees
+where last_name like 'e%' and last_name like '%e'
+group by first_name, last_name;
+
+select  last_name, count(*)
+from employees
+where last_name like '%q%' and last_name not like '%qu%'
+group by last_name
+order by count(*);
+
+select gender, count(*)
+from employees
+where (first_name = 'Irena'
+    or first_name='Vidya'
+    or first_name='Maya')
+  group by gender
+order by count(*);
+
+
+
+
+
